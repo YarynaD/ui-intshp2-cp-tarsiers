8180496e1741f23905feb54c964dffa6c13fba6d
YarynaD <jaryna.m@gmail.com>
2019-01-11 16:36:50 +0200

    Advertising area component

--- src/app/components/AdvertisingArea/AdvertisingArea.js
+++ src/app/components/AdvertisingArea/AdvertisingArea.js
@@ -1,28 +1,68 @@
 import React from 'react';
 import HttpService from '../../../utils/http.service';
 import appConfig from '../../../config/appConfig';
+import Dynamic from './Dynamic';
+import './Banner.scss';
+
+const sanitizeHtml = require('sanitize-html');
 
 export default class AdvertisingArea extends React.Component {
   constructor() {
     super();
-    this.state = { htmlSnipet: '' };
+    this.state = {
+      htmlSnipet: ''
+    };
   }
 
   componentDidMount() {
     HttpService.get(appConfig.apiResources.baner).then(response => {
-      this.setState({ htmlSnipet: response.htmlSnipet });
+      const clean = this.mySanitize(response.htmlSnipet);
+
+      this.setState({ htmlSnipet: clean });
     });
   }
 
-  createMarkup = () => {
-    const { htmlSnipet } = this.state;
+  mySanitize(dirty) {
+    const myClean = sanitizeHtml(dirty, {
+      allowedTags: [
+        'b',
+        'i',
+        'em',
+        'strong',
+        'a',
+        'div',
+        'p',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'img',
+        'style'
+      ],
+      allowedAttributes: { a: ['href'], img: ['src'] },
+      allowedIframeHostnames: ['www.youtube.com'],
+      allowedClasses: {
+        p: ['item__description', 'simple'],
+        div: ['banner', 'item', 'item__description', 'priceblock'],
+        h3: ['item__name'],
+        h2: ['priceblock__price'],
+        img: ['banner__image', 'priceblock__cart']
+      },
+      nonTextTags: ['style']
+    });
 
-    return { __html: `<p>${htmlSnipet}</p>` };
-  };
+    return myClean;
+  }
 
   render() {
-    const { htmlSnipet } = this.state;
+    const mySnipet = this.state;
 
-    return htmlSnipet ? <div>{htmlSnipet}</div> : null;
+    return (
+      <div className="adArea">
+        <Dynamic html={mySnipet.htmlSnipet} />
+      </div>
+    );
   }
 }
--- src/app/components/HomePage/HomePage.js
+++ src/app/components/HomePage/HomePage.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import JoinUs from '../JoinUs/JoinUs';
 import Promotions from '../Promotions/Promotions';
-import Banner from '../Banner/Banner';
 import AdvertisingArea from '../AdvertisingArea/AdvertisingArea';
 
 const HomePage = () => (
-  <div className="col-9 slider__wrapper">
+  <div>
     <Promotions />
     <JoinUs />
     <AdvertisingArea />
-    <Banner />
   </div>
 );
 
